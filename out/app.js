9eb70aac6acb2f558a5cb592a088542ee7edbc54
j-carlander <johannes.carlander@gmail.com>
2023-08-22 11:02:02 +0200

    Function to append each contact in contacts as a li to the ul

--- out/app.js
+++ out/app.js
@@ -1,10 +1,23 @@
 "use strict";
 const addContactForm = document.querySelector(".add-contact-form");
 const contactField = document.querySelector(".contact-field");
+const contactsList = document.querySelector(".contacts-list");
 const contacts = [];
+function createListItem(item) {
+    let li = document.createElement("li");
+    li.innerText = item;
+    return li;
+}
+function appendToContactsList(contacts) {
+    contactsList.innerHTML = "";
+    contacts.forEach((contact) => contactsList.append(createListItem(contact)));
+}
+// if the contacts list is not empty on first run, append the contacts to the Contacts list in DOM
+if (contacts.length > 0)
+    appendToContactsList(contacts);
 addContactForm.addEventListener("submit", (e) => {
     e.preventDefault();
     contacts.push(contactField.value);
-    console.log(contacts);
+    appendToContactsList(contacts);
     contactField.value = "";
 });
--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,31 @@ const addContactForm: HTMLFormElement = document.querySelector(
 const contactField: HTMLInputElement = document.querySelector(
   ".contact-field"
 ) as HTMLInputElement;
+const contactsList: HTMLUListElement = document.querySelector(
+  ".contacts-list"
+) as HTMLUListElement;
 
 const contacts: string[] = [];
+
+function createListItem(item: string): HTMLLIElement {
+  let li: HTMLLIElement = document.createElement("li");
+  li.innerText = item;
+  return li;
+}
+
+function appendToContactsList(contacts: string[]): void {
+  contactsList.innerHTML = "";
+  contacts.forEach((contact: string): void =>
+    contactsList.append(createListItem(contact))
+  );
+}
+
+// if the contacts list is not empty on first run, append the contacts to the Contacts list in DOM
+if (contacts.length > 0) appendToContactsList(contacts);
 
 addContactForm.addEventListener("submit", (e): void => {
   e.preventDefault();
   contacts.push(contactField.value);
-  console.log(contacts);
+  appendToContactsList(contacts);
   contactField.value = "";
 });
